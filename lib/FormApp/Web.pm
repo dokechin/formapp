af1040b188c69ec5fc0beaec77f3103fd2f0b0d7
Tetsuya Tatsumi <ttatsumi@ra2.so-net.ne.jp>
2013-03-23 04:33:41 +0900

    first commit

---
+++ lib/FormApp/Web.pm
@@ -0,0 +1,19 @@
+package FormApp::Web;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Documentation browser under "/perldoc"
+  $self->plugin('PODRenderer');
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/')->to('example#welcome');
+  $r->post('/newentry')->to('example#newentry');
+}
+
+1;
